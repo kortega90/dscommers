5e5760656d628ccbad72d32282041b327ab88a18
kortega90 <kortega90@hotmail.com>
2022-12-01 08:50:00 -0300

    first commit

---
+++ src/main/java/com/kortega/dscommerce/entities/OrderStatus.java
@@ -0,0 +1,9 @@
+package com.kortega.dscommerce.entities;
+
+public enum OrderStatus {
+    WAITING_PAYMENT,
+    PAID,
+    SHIPPED,
+    DELIVERED,
+    CANCELED;
+}
---
+++ src/test/java/com/kortega/dscommerce/DscommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kortega.dscommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DscommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
