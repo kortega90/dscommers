77723f2c7bfd8931c682224b22d4b9868689f16a
kortega90 <kortega90@hotmail.com>
2022-12-05 16:08:51 -0300

    POST ,PUT GET

--- src/main/java/com/kortega/dscommerce/controllers/ProductController.java
+++ src/main/java/com/kortega/dscommerce/controllers/ProductController.java
@@ -1,28 +1,42 @@
 package com.kortega.dscommerce.controllers;
 
 import com.kortega.dscommerce.dto.ProductDTO;
-import com.kortega.dscommerce.entities.Product;
 import com.kortega.dscommerce.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import java.util.Optional;
+import java.net.URI;
+import java.util.List;
 
 @RestController
-@RequestMapping (value = "/products")
+@RequestMapping(value = "/products")
 public class ProductController {
 
     @Autowired
     private ProductService service;
 
     @GetMapping(value = "/{id}")
-    public ProductDTO findById(@PathVariable Long id){
-ProductDTO dto =service.findById(id);
-      return dto;
+    public ResponseEntity <ProductDTO> findById(@PathVariable Long id) {
+        ProductDTO dto = service.findById(id);
+        return ResponseEntity.ok(dto);
     }
+
+    @GetMapping
+    public ResponseEntity<Page<ProductDTO>> findAll (Pageable pageable){
+        return ResponseEntity.ok(service.findByAll(pageable));
+    }
+
+    @PostMapping
+    public ResponseEntity<ProductDTO> insert (@RequestBody ProductDTO dto){
+        dto = service.insert(dto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
+        return ResponseEntity.created(uri).body(dto);
+    }
+
 
 }
 
--- src/main/java/com/kortega/dscommerce/services/ProductService.java
+++ src/main/java/com/kortega/dscommerce/services/ProductService.java
@@ -4,9 +4,12 @@ import com.kortega.dscommerce.dto.ProductDTO;
 import com.kortega.dscommerce.entities.Product;
 import com.kortega.dscommerce.repositories.ProductRespository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -15,11 +18,54 @@ public class ProductService {
     @Autowired
     private ProductRespository respository;
 
-@Transactional(readOnly = true)
-    public ProductDTO findById (Long id) {
-        Optional <Product> result = respository.findById(id);
+    @Transactional(readOnly = true)
+    public ProductDTO findById(Long id) {
+        Optional<Product> result = respository.findById(id);
         Product product = result.get();
         ProductDTO dto = new ProductDTO(product);
-        return  dto;
+        return dto;
+
+        /* ou *****
+        public ProductDTO findById(Long id) {
+        Product product = respository.findById(id).get();
+      return = new ProductDTO(product);
+         */
+    }
+
+    @Transactional(readOnly = true)
+    public Page<ProductDTO> findByAll(Pageable pageable) {
+        Page<Product> result = respository.findAll(pageable);
+        return result.map(x -> new ProductDTO(x));
+    }
+
+    @Transactional
+    public ProductDTO insert(ProductDTO dto) {
+        Product prod = new Product();
+        prod.setName(dto.getName());
+        prod.setDescription(dto.getDescription());
+        prod.setPrice(dto.getPrice());
+        prod.setImgUrl(dto.getImgUrl());
+
+        /*
+        prod = respository.save(prod);
+        return new ProductDTO(prod);
+        */
+        return new ProductDTO(respository.save(prod));
+    }
+
+    @Transactional
+    public ProductDTO Update(Long id, ProductDTO dto) {
+
+        Product prod = respository.getReferenceById(id);
+        /* outra opçaõ utilizando o metodo*/
+        copyDtoToProd (dto, prod)
+        return new ProductDTO(respository.save(prod));
+    }
+
+    private void copyDtoToProd(ProductDTO dto, Product prod) {
+        prod.setName(dto.getName());
+        prod.setDescription(dto.getDescription());
+        prod.setPrice(dto.getPrice());
+        prod.setImgUrl(dto.getImgUrl());
     }
 }
