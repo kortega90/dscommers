0a19ae1aa59435472b23c03d0ab05ffa91e4e22c
kengi ortega <kortega90@hotmail.com>
2023-01-31 16:42:49 -0300

    Access control

--- src/main/java/com/kortega/dscommerce/controllers/ProductController.java
+++ src/main/java/com/kortega/dscommerce/controllers/ProductController.java
@@ -2,16 +2,16 @@ package com.kortega.dscommerce.controllers;
 
 import com.kortega.dscommerce.dto.ProductDTO;
 import com.kortega.dscommerce.services.ProductService;
-import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.validation.Valid;
 import java.net.URI;
-import java.util.List;
 
 @RestController
 @RequestMapping(value = "/products")
@@ -32,6 +32,7 @@ public class ProductController {
         return ResponseEntity.ok(service.findByAll(name, pageable));
     }
 
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     @PostMapping
     public ResponseEntity<ProductDTO> insert(@Valid @RequestBody ProductDTO dto) {
         dto = service.insert(dto);
@@ -39,12 +40,13 @@ public class ProductController {
         return ResponseEntity.created(uri).body(dto);
     }
 
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     @PutMapping(value = "/{id}")
     public ResponseEntity<ProductDTO> update(@PathVariable Long id, @Valid @RequestBody ProductDTO dto) {
         dto = service.update(id, dto);
         return ResponseEntity.ok(dto);
     }
-
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     @DeleteMapping(value = "/{id}")
     public ResponseEntity<Void> delete(@PathVariable Long id) {
         service.delete(id);
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -58,8 +58,9 @@ INSERT INTO tb_product_category (product_id, category_id) VALUES (25, 3);
 INSERT INTO tb_user (name, email, phone, password, birth_date) VALUES ('Maria Brown', 'maria@gmail.com', '988888888', '$2a$10$Iq4AXqwRdj5KtH6XCHEmeOMQ7SbF7M9GFevRERRa6AQ5uFL827ZMO', '2001-07-25');
 INSERT INTO tb_user (name, email, phone, password, birth_date) VALUES ('Alex Green', 'alex@gmail.com', '977777777', '$2a$10$Iq4AXqwRdj5KtH6XCHEmeOMQ7SbF7M9GFevRERRa6AQ5uFL827ZMO', '1987-12-13');
 
-INSERT INTO tb_role (authority) VALUES ('ROLE_ADMIN');
 INSERT INTO tb_role (authority) VALUES ('ROLE_CLIENT');
+INSERT INTO tb_role (authority) VALUES ('ROLE_ADMIN');
+
 
 INSERT INTO tb_user_role (user_id,role_id) VALUES (1,1);
 INSERT INTO tb_user_role (user_id,role_id) VALUES (2,1);
